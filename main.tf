17975776ef8f0d9cd578f250ec462f5a13d96eb4
unknown <adelson.hdez@gmail.com>
2021-10-25 15:27:54 -0600

    Add file

---
+++ main.tf
@@ -0,0 +1,18 @@
+#deploy a Kubernetes
+resource "digitalocean_kubernetes_cluster" "focus" {
+  name    = "focus"
+  region  = "nyc1"
+  version = "1.21.3-do.0"
+
+  tags = ["my-tag"]
+
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-1vcpu-2gb"
+    auto_scale = false
+    node_count = 1
+    tags       = ["node-pool-tag"]
+  }
+
+}
+
